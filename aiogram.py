5b0d90be67cd05c8d0a0ef3514cb76e90538a109
ushastoe <40743392+krolchonok@users.noreply.github.com>
2024-03-04 09:09:04 +0300

    Create aiogram.py

---
+++ aiogram.py
@@ -0,0 +1,19 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher, Router, types
+
+TOKEN = ""
+
+dp = Dispatcher()
+
+@dp.message()
+async def echo_handler(message: types.Message) -> None:
+    ...
+  
+async def main() -> None:
+    bot = Bot(TOKEN)
+    await dp.start_polling(bot)
+
+logging.basicConfig(level=logging.INFO)
+asyncio.run(main())
